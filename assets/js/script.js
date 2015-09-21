471d0d8a98edad02b58e8327e3c886d4fdf7f501
illia-k <illia.kyeylenko@gmail.com>
2015-09-21 14:47:44 +0300

    Add delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@
 /// <reference path="lib/underscore-min.js" />
 
 $(document).ready(function () {
-
 	var Item = Backbone.Model.extend({
 		defaults: {
 			part1: 'hello',
-			part2: 'world'
+			part2: 'world',
+			id: 0			
 		}
 	});
 
@@ -18,14 +18,26 @@ $(document).ready(function () {
 	var ItemView = Backbone.View.extend({
 
 		tagName: 'li', // name of (orphan) root tag in this.el
+		events: {
+			'click button.del': 'delSelf'
+		},
+		
 
 		initialize: function () {
-			_.bindAll(this, 'render'); // every function that uses 'this' as the current object should be in here
+			_.bindAll(this, 'render', 'delSelf'); // every function that uses 'this' as the current object should be in here
 		},
 
 		render: function () {
-			$(this.el).html('<span>' + this.model.get('part1') + ' ' + this.model.get('part2') + '</span>');
+			$(this.el).html(
+				'<span id="' + this.model.get('id') + '">' 
+					+ this.model.get('part1') + ' ' 
+					+ this.model.get('part2') + '</span>'
+				+ '<button class="del" item="' + this.model.get('id') + '">Del</button>');
 			return this; // for chainable calls, like .render().el
+		},
+		
+		delSelf: function(){
+			this.remove();
 		}
 	});
 
@@ -33,7 +45,7 @@ $(document).ready(function () {
 
 		el: $('body'), // el attaches to existing element
 		events: {
-			'click button#add': 'addItem'
+			'click button#add': 'addItem'			
 		},
 
 		initialize: function () {
@@ -59,16 +71,19 @@ $(document).ready(function () {
 			this.counter++;
 			var item = new Item();
 			item.set({
-				part2: item.get('part2') + this.counter // modify item defaults
+				part2: item.get('part2') + this.counter, // modify item defaults
+				id: this.counter
 			});
 			this.collection.add(item);
 		},
+		
 		appendItem: function (item) {
 			var itemView = new ItemView({
 				model: item
 			});
 			$('ul', this.el).append(itemView.render().el);
-		}
+		}		
+
 	});
 
 
